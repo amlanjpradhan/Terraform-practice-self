adf65e03e9e76dcf12c0a68787aad18637b076b8
amlanjpradhan <amlanjyoti.pradhan@gmail.com>
2026-05-26 12:11:48 +0530

    day2

---
+++ day2-configurationfiles/terraform.tfvars
@@ -0,0 +1,2 @@
+ ami_id = "ami-024050934c1c66b27"
+ type = "t3.micro"
---
+++ day2-configurationfiles/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    description = "passing AMI value"
+    default = ""
+    type = string
+  
+}
+variable "type" {
+    description = "value for instance type"
+    default =""
+    type = string
+
+  
+}
